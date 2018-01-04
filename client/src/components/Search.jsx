c9606392318a5b226b1cfbe9a8f44c72092851aa
Albert Chang <albertchanged@gmail.com>
2018-01-03 23:16:50 -0800

    Enable Search.jsx to autofill select options with Ticketmaster Venue data!

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
+import $ from 'jquery';
+var config = require('.../../../config.js');
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      locations: [{'id': 1, 'name': 'San Francisco, CA'}, {'id': 2, 'name': 'Los Angeles, CA'}],
       genre: '',
       city: ''
     }
+  }
+  componentDidMount() {
+    $.ajax({
+      url: `https://app.ticketmaster.com/discovery/v2/venues.json?&apikey=${config.KEY}`,
+      type: 'GET',
+      async: true,
+      dataType: "json",
+      success: (data) => {
+        console.log('GET success: ', data._embedded);
+        var venues = data._embedded.venues;
+        console.log(venues);
+        var locationArray = [];
+        venues.forEach((venue) => {
+          var locationObj = {
+            'id': venue.id,
+            'city': venue.city.name,
+            'state': venue.country.countryCode
+          }
+          locationArray.push(locationObj);
+        })
+        this.setState({
+          locations: locationArray
+        })
+        // this.getRepos();
+      },
+      error: (data) => {
+        console.log('GET error: ', data);
+      }
+    });
   }
   handleSubmit(event) {
     event.preventDefault();
@@ -27,7 +59,11 @@ class Search extends React.Component {
             <input id="genreInput" type="text" />
             <h3>Specify a city!</h3>
             <select id="cityDropdown">
-              
+              {
+                this.state.locations.map((location) => (
+                  (<option key={location.id} value={location.city + ', ' + location.state}>{location.city + ', ' + location.state}</option>)
+                ))
+              }
             </select>
             <input id="cityInput" type="text" />
             <button onClick={this.sendData.bind(this)}>Search!</button>
--- database/schema.sql
+++ database/schema.sql
@@ -8,15 +8,15 @@ CREATE TABLE locations(
   state VARCHAR(20) NOT NULL,
   country VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE events(
   id INT AUTO_INCREMENT,
-  name VARCHAR(125) NOT NULL,
+  name VARCHAR(105) NOT NULL,
   state VARCHAR(20) NOT NULL,
   country VARCHAR(30) NOT NULL,
   genre VARCHAR(25) NOT NULL,
   PRIMARY KEY (id)
-)
+);
 
--- mysql -u root 
+-- mysql -u root < database/schema.sql --
